e545f037d2526e009c11970f0a233372d18fa1c0
Nirel Chua <95299412+cninz@users.noreply.github.com>
2023-09-09 09:13:45 -0600

    Screens

---
+++ screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { View, Text, Button } from "react-native";
+
+const HomeScreen = () => {
+    const navigation = useNavigation()
+
+    return (
+        <View>
+            <Text>I am the HomeScreen</Text>
+            <Button
+                title="Go to Chat Screen"
+                onPress={() => navigation.navigate('Chat')}
+            />
+        </View>
+    )
+}
+
+export default HomeScreen;
